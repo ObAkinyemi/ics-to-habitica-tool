0b1715827d7bc6e57781f073df5c0613162e6a58
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-09-05 22:55:28 -0600

    dotenv is not working and I don't know why my fetch url is incorrect. I can't put my username into the fetch url, I'm supposed ot just use the url as it is in the api docs. I didn't know that. They should have specified that.

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
-import dotenv from 'dotenv'
+// import dotenv as 'dotenv'
 
-xXclientXx = process.env.USER_ID;
-X_api_key = process.env.API_KEY;
-uid = process.env.USER_ID;
+const headers = new Headers();
+headers.append("x-client", "a52cd6ba-b2c4-4a6a-829e-aa407525c4aa-ics-to-habitica-tool");
+headers.append("x-api-key", "a4effcb3-e090-4201-a2e8-a83cd78aee77");
+headers.append("x-api-user", "a52cd6ba-b2c4-4a6a-829e-aa407525c4aa");
+
+// const xXclientXx = process.env.USER_ID;
+// const X_api_key = process.env.API_KEY;
+// const uid = process.env.USER_ID;
+
 console.log("Hi");
 test();
-// test().then((data) => {
-//     console.log(data);
-//     return data;
-// }).catch((err) => {
-//     console.error(err);
-    
-// });
+
 async function test() {
     try {
-        const xXclientXx = new Headers();
-        xXclientXx.append("X_Client",  );
-        const res = await fetch(`https://habitica.com/api/v3/tasks/The_Great_Saiyenmam`, {
+
+        const res = await fetch("https://habitica.com/api/v3/tasks/the_Great_Saiyenmam ", {
             method: "GET",
-            headers: {
-                "x-client": "l",
-                "x-api-key": "a",
-                "x-api-user": "aa",
-            }
+            headers: headers,
+
         });
+        if(!res.ok){
+            console.error("you can't get the user's tasks.");
+        }
         const data = await res.json();
         console.log(data);
-        // return data;
-        
     } catch (error) {
-        // error = data.error;
-        // let errorMessage = data.message;
-        // console.error(`${error}:\n${errorMessage}`);
         console.error(error);
     }
 }
+
+async function getUserProfile() {
+  try {
+    const response = await fetch("https://habitica.com/api/v3/the_Great_Saiyenmam", {
+      method: "GET",
+      headers: headers,
+      
+    });
+
+    if (!response.ok) {
+      // If the response is not successful, throw an error
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("Successfully fetched user data:", data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+  }
+}
+
+// Call the function to run it
+// getUserProfile();
