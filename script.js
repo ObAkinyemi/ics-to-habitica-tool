01652d525031e88bdaf46c15badf4df9825bd51d
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-09-05 22:59:32 -0600

    note to self. I should ALWAYS use the api call url as it is in the response fetch statement whenever I'm using an API. My code is fixed now, but it took 3 hours.

--- script.js
+++ script.js
@@ -9,47 +9,20 @@ headers.append("x-api-user", "a52cd6ba-b2c4-4a6a-829e-aa407525c4aa");
 // const X_api_key = process.env.API_KEY;
 // const uid = process.env.USER_ID;
 
-console.log("Hi");
-test();
+getUserTasks();
 
-async function test() {
+async function getUserTasks() {
     try {
-
-        const res = await fetch("https://habitica.com/api/v3/tasks/the_Great_Saiyenmam ", {
+        const res = await fetch("https://habitica.com/api/v3/tasks/user", {
             method: "GET",
             headers: headers,
-
         });
-        if(!res.ok){
-            console.error("you can't get the user's tasks.");
-        }
         const data = await res.json();
-        console.log(data);
+        console.log(data.data);
+        if(!res.ok){
+            console.error("a problem with the http request");
+        }
     } catch (error) {
         console.error(error);
     }
 }
-
-async function getUserProfile() {
-  try {
-    const response = await fetch("https://habitica.com/api/v3/the_Great_Saiyenmam", {
-      method: "GET",
-      headers: headers,
-      
-    });
-
-    if (!response.ok) {
-      // If the response is not successful, throw an error
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log("Successfully fetched user data:", data);
-    return data;
-  } catch (error) {
-    console.error("Error fetching user profile:", error);
-  }
-}
-
-// Call the function to run it
-// getUserProfile();
